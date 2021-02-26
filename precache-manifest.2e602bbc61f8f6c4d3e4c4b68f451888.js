8b235172e10f263de4fa04d7e8b30500bba67e2a
JAYMAINA <jaymaina98@gmail.com>
2021-02-26 21:06:45 +0300

    hi

---
+++ precache-manifest.2e602bbc61f8f6c4d3e4c4b68f451888.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78455e3eabb222ba4b0beab33b7a42d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a0b7f5284d57d108c2",
+    "url": "/static/css/main.a04c5505.chunk.css"
+  },
+  {
+    "revision": "32abe248b9f000636978",
+    "url": "/static/js/2.871ac47e.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/static/js/2.871ac47e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a0b7f5284d57d108c2",
+    "url": "/static/js/main.364ac264.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "e9a77e7bdcf4262b0d7ed230c0042f83",
+    "url": "/static/media/100-fireworks.e9a77e7b.gif"
+  },
+  {
+    "revision": "1a3f647371ec52effcf63c0f1a4cac84",
+    "url": "/static/media/Echizen.1a3f6473.ttf"
+  },
+  {
+    "revision": "d31c69da78ed9f1d54ef33c2cb91df8e",
+    "url": "/static/media/add2.d31c69da.mp3"
+  },
+  {
+    "revision": "9ea9d06a5b3906c3626d3c51ed37e0ad",
+    "url": "/static/media/cassette.9ea9d06a.png"
+  },
+  {
+    "revision": "ea8e806357c06cc76c7fe89fcc31945d",
+    "url": "/static/media/correct.ea8e8063.mp3"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/static/media/digital-7.regular.1e670d88.ttf"
+  },
+  {
+    "revision": "0f4d71c11ab99cf63118bcdb9d11ffb9",
+    "url": "/static/media/jamie.0f4d71c1.svg"
+  },
+  {
+    "revision": "120df319d809b46e0d4b6ac7766446ae",
+    "url": "/static/media/remove2.120df319.mp3"
+  },
+  {
+    "revision": "0c5e0bb902401a08016a6906307e38b7",
+    "url": "/static/media/success.0c5e0bb9.mp3"
+  },
+  {
+    "revision": "0119cb902f905dcb46b07f705f043aaa",
+    "url": "/static/media/textured_background_green.0119cb90.jpg"
+  },
+  {
+    "revision": "47687ae3ad5bd216cee9f59a2ec6a7d9",
+    "url": "/static/media/wrong.47687ae3.mp3"
+  },
+  {
+    "revision": "89c0c5ef1968249b37fdb574f34618a7",
+    "url": "/static/media/y.89c0c5ef.svg"
+  }
+]);
